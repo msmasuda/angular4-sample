b53a2d47fbfd3aae8cc87d68df4175cc124130bc
masuda <masuda@my-style.jp>
2017-06-12 00:57:35 +0900

    一旦コミット

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
 import { Comment, User } from './class/chat';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import $ from 'jquery/dist/jquery';
 
 const CURRENT_USER: User = new User(1, 'Shinichi Masuda');
-const ANOTHER_USER: User = new User(2, 'Taro Yamada');
-const COMMENTS: Comment[] = [
-    new Comment(ANOTHER_USER, 'Yamadaの１つ目のコメントです。'),
-    new Comment(ANOTHER_USER, 'Yamadaの２つ目のコメントです。'),
-    new Comment(CURRENT_USER, 'Masudaの１つ目のコメントです。'),
-    new Comment(ANOTHER_USER, 'Yamadaの３つ目のコメントです。'),
-    new Comment(CURRENT_USER, 'Masudaの２つ目のコメントです。')
-  ]
 
 @Component({
   selector: 'app-root',
@@ -19,14 +12,16 @@ const COMMENTS: Comment[] = [
 })
 
 export class AppComponent {
-  title = 'Angular4 Sample';
+  title = 'チャットサンプル';
   items: FirebaseListObservable<any[]>;
   public content = '';
-  public comments = COMMENTS;
   public current_user = CURRENT_USER;
 
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/items');
+    $('#exampleModal').on('shown.bs.modal', () => {
+      $('#recipient-name').focus()
+    });
   }
 
   save(comment: string) {
