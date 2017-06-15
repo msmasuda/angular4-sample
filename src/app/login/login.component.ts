86eab9cdd7d4eff107425a839190c68838a63abf
Shinichi Masuda <masuda@systemplanet.co.jp>
2017-06-15 15:03:09 +0900

    Modify

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 @Component({
@@ -22,6 +22,19 @@ export class LoginComponent implements OnInit {
 
   login() {
     firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+    .then((user) => {
+      console.log(user);
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      // var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
+
+  create() {
+    firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
     .then((user) => {
       console.log(user);
     })
